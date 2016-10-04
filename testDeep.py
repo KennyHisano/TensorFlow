8a93c6ace91b28f887a02381a7815aa4aeba6fc2
Kenny H <nobi3892@gmail.com>
2016-10-04 13:46:36 +0900

    done with lexicon

--- testDeep.py
+++ testDeep.py
@@ -29,42 +29,43 @@ batch_size=100
 #set of batch is a small bucket to store data
 
 
-x = tf.placeholder('float'[None, 784])#heightx width it shows error when it goes over this limit
-y = tf.placeholder('float')
+x = tf.placeholder("float",[None, 784])
+#heightx width it shows error when it goes over this limit
+y = tf.placeholder("float")
 
-def = neutak_network_model(data):
+def  neural_network_model(data):
 
 
 	#(input_data * weights) + bias
 	#weights = strength between two nodes
 	hidden_1_layer = {'weights':tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-		'biases':tf.Variable(tf.random_normal(n_nodes_hl1))} 
+		'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))} 
 
 	hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-		'biases':tf.Variable(tf.random_normal(n_nodes_hl2))} 
+		'biases':tf.Variable(tf.random_normal([n_nodes_hl2]))} 
 
 	hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-		'biases':tf.Variable(tf.random_normal(n_nodes_hl3))} 
+		'biases':tf.Variable(tf.random_normal([n_nodes_hl3]))} 
 
 	output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_class])),
 		'biases':tf.Variable(tf.random_normal([n_class]))} 
 
-	l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']) + hidden_1_layer['biases']) 
+	l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']) , hidden_1_layer['biases']) 
 	l1 = tf.nn.relu(l1)
 
-	l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']) + hidden_2_layer['biases']) 
+	l2 = tf.add(tf.matmul(l1,hidden_2_layer['weights']) ,hidden_2_layer['biases']) 
 	l2 = tf.nn.relu(l2)
 
-	l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']) + hidden_3_layer['biases']) 
+	l3 = tf.add(tf.matmul(l2,hidden_3_layer['weights']) , hidden_3_layer['biases']) 
 	l3 = tf.nn.relu(l3)
 
-	output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']) 
+	output = tf.matmul(l3,output_layer['weights']) + output_layer['biases']
 
 	return output
 
 
 def train_neural_network(x):
-	prediction = neutak_network_model(x)
+	prediction = neural_network_model(x)
 	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction,y))
 
 	#minimize
@@ -73,6 +74,29 @@ def train_neural_network(x):
 
 	#cycle feed forward and back prop
 	hm_epochs = 10
+
+
+	with tf.Session() as sess:
+		sess.run(tf.initialize_all_variables())
+
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			#it tells us the needed how many times we need to cycle
+			for _ in range(int(mnist.train.num_examples/batch_size)):
+				epoch_x, epoch_y = mnist.train.next_batch(batch_size)
+				_, c = sess.run([optimizer, cost], feed_dict = {x: epoch_x,y: epoch_y})
+				epoch_loss += c
+			print('Epoch',epoch, 'completed out of', hm_epochs,'loss:',epoch_loss)
+
+			#argmax = return index of the max value 
+		correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
+		accuracy = tf.reduce_mean(tf.cast(correct,'float'))
+		print('Accuracy:', accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+
+
+train_neural_network(x)
+
 
 '''
 	In machine-learning parlance, an epoch is a complete pass through a given dataset.
@@ -85,28 +109,6 @@ def train_neural_network(x):
 	     Say your data has 2 minibatches: A and B. .numIterations(3) performs training like AAABBB, 
 	     while 3 epochs looks like ABABAB.
 '''
-
-	with tf.Session() as sess:
-		sess.run(tf.initalize_all_variables())
-
-		for epoch in hm_epochs
-			epoch_loss = 0
-			#it tells us the needed how many times we need to cycle
-			for _ in range(int(mnist.train.num_examples/batch_size)
-				x, y = mnist.train.next_batch(batch_size)
-				_, c = sess.run([optimizer, cost], feed_dict = {x: x,y: y})
-				epoch_loss += c
-			print('Epoch',epoch, 'completed out of', hm_epochs,'loss:',epoch_loss)
-
-			#argmax = return index of the max value 
-		correct = tf.equal(tf.argmax(prediction,1), tf.argmax(y,1))
-		accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-		print('Accuracy:', accuracy.eval({x:mist.test.images, y:mnist.test.labels}))
-
-
-
-train_neural_network(x)
-
 
 
 
